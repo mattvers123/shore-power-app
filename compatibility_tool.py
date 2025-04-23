19a211c931f2035f8b4e2361f6a32ff64dc57ce4
mattvers123 <50327772+mattvers123@users.noreply.github.com>
2025-04-23 19:02:22 +0200

    Update compatibility_tool.py

--- compatibility_tool.py
+++ compatibility_tool.py
@@ -55,10 +55,8 @@ if st.session_state.show_analysis:
     try:
     	param_config_sheet = client.open("Bluebarge_Comp_Texts").worksheet("Analysis")
     	param_config_df = pd.DataFrame(param_config_sheet.get_all_records())
-
-    	
-        st.subheader("All Compatibility Parameters")
-        st.dataframe(param_config_df)
+ 			st.subheader("All Compatibility Parameters")
+    	st.dataframe(param_config_df)
 
     except Exception as e:
     	st.warning(f"Could not load editable parameter definitions: {e}")
