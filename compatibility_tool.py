399f1719f98f65998d730c447e309f713544b61d
KadirOZY <94007296+KadirOZY@users.noreply.github.com>
2025-06-11 15:21:51 +0300

    Update compatibility_tool.py

--- compatibility_tool.py
+++ compatibility_tool.py
@@ -9,8 +9,6 @@ import tempfile
 import matplotlib.pyplot as plt
 import numpy as np
 import requests
-import folium
-from streamlit_folium import st_folium
 from datetime import datetime, timedelta
 from timezonefinder import TimezoneFinder
 import pytz
