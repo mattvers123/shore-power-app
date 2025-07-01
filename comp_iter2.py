223419c184dc01381a261b07d248d4e57c99e0f4
mattvers123 <50327772+mattvers123@users.noreply.github.com>
2025-07-01 12:13:30 +0300

    Update comp_iter2.py

--- comp_iter2.py
+++ comp_iter2.py
@@ -841,8 +841,7 @@ if st.session_state.show_analysis:
             include = st.checkbox("Include?", value=st.session_state[key], key=key)
             st.session_state[key] = include  # update state
 
-            
-
+        
             if not row.get("Selection", False):
                 continue
 
@@ -879,9 +878,10 @@ if st.session_state.show_analysis:
                 min_value=0.0,
                 value=1.0,
             )
-            if name_key == "power capacity match":
+            
+            if param_name == "power capacity match":
                 provided = 6.5  # or whatever barge value you want
-            elif name_key == "energy autonomy":
+            elif param_name == "energy autonomy":
                 provided = 30.0  # or your sample barge value
         
             score = compute_score_contribution(required, provided, weight)
